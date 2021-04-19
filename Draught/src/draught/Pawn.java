6d4f1cc6bfda310f57f5f1066aebfd3b869ca1a3
balintovics <molnar99b@gmail.com>
2021-04-19 14:14:00 +0200

    skeleton Pawn

--- Draught/src/draught/Pawn.java
+++ Draught/src/draught/Pawn.java
@@ -1,4 +1,28 @@
 package draught;
 
 public class Pawn {
+    String color;
+    boolean isCrowned = false;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public class Coordinates {
+        int x;
+        int y;
+    }
+
+    private boolean getIsWhite() {
+        return this.color.equals("white");
+    }
+
+    private boolean getIsCrowned() {
+        return this.isCrowned;
+    }
+
 }
